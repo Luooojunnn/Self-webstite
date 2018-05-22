bbac5d2aa5eb2d13e4a4e06a2ca7f556150908ff
Luooojunnn <iyoloyolo@163.com>
2018-05-22 15:28:17 +0800

    Update chuandaoyemian.php

--- works/pages/chuandaoyemian.php
+++ works/pages/chuandaoyemian.php
@@ -5,7 +5,7 @@
 	//echo '阿里云虚拟机数据库连接不上，过两天再弄';
 
 	if( isset($_GET['page']) && !empty($_GET['page']) ){
-		$conn = mysqli_connect( 'bdm262229207.my3w.com','bdm262229207','woaishuxue123456','bdm262229207_db' ) or die('服务器链接失败');
+		$conn = mysqli_connect( 'bdm262229207.my3w.com','用户名','密码','数据库名字' ) or die('服务器链接失败');
 		mysqli_query($conn,'set names utf8');
 		mysqli_select_db( $conn , 'bdm262229207_db' ) or die( '传到页面数据库链接失败' );
 		$get_page = "SELECT * FROM pages WHERE id = $page";
